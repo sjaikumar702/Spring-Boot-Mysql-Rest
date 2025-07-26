7d62c8688f56052b78f991a1581c024cde2c3025
jaikumar S <sjaikumar702@gmail.com>
2025-07-26 21:12:52 +0530

    Added changes and indentation

--- src/main/java/com/springboot/repository/CourseRepository.java
+++ src/main/java/com/springboot/repository/CourseRepository.java
@@ -2,9 +2,6 @@ package com.springboot.repository;
 
 import com.springboot.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.io.Serializable;
-//@Repository //-->optional
 public interface CourseRepository extends JpaRepository<Course, Serializable> {
 }
